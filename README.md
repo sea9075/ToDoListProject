1c96c7c1ec7a5725d6bdab1ee79286b7072b2ede
KUEI <sea9075@gmail.com>
2024-06-18 20:50:07 +0800

    20240618

---
+++ README.md
@@ -0,0 +1,37 @@
+# TodoList Demo
+## 設計簡單的 TodoList
+包含簡單的功能
+* 總攬
+* 新增
+* 修改
+* 刪除
+## 總攬
+* 可以查看所有的代辦事項
+## 新增
+* 可以新增新的代辦事項
+* 包含以下設定項
+    - 標題 [必要]
+    - 描述 [非必要]
+    - 狀態 [必要，選單，預設啟動]
+    - 開始時間 [必要]
+    - 結束時間 [非必要]
+    - 建立時間 [預設輸入當前時間，無法修改]
+## 修改
+* 可以修改代辦事項
+* 包含以下設定
+    - 標題[必要，可修改]
+    - 描述[非必要，可修改]
+    - 狀態[必要，選單]
+    - 開始時間[必要，可修改]
+    - 結束時間[非必要，可修改]
+    - 建立時間[必要，不可修改]
+## 刪除
+* 可刪除已建立的代辦事項
+## 參考資料
+[ASP.NET Core](https://learn.microsoft.com/zh-tw/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-8.0&tabs=visual-studio)
+
+[ChatGPT](https://chatgpt.com/)
+
+[Bootswatch](https://bootswatch.com/)
+
+[Toastr](https://codeseven.github.io/toastr/)
--- TodoListDemo/TodoListDemo/Controllers/TodoListController.cs
+++ TodoListDemo/TodoListDemo/Controllers/TodoListController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 using TodoListDemo.Data;
 using TodoListDemo.Models;
 
@@ -18,13 +19,91 @@ namespace TodoListDemo.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var showTime = new TodoList
+            {
+                Status = true,
+                StartTime = DateTime.Now,
+                CreateTime = DateTime.Now
+            };
+            return View(showTime);
         }
         [HttpPost]
         public IActionResult Create(TodoList obj)
         {
-            _db.todoLists.Add(obj);
+            if (obj.Title == null)
+            {
+                TempData["error"] = "請輸入標題";
+                return RedirectToAction("Create");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.todoLists.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "代辦事項新增成功";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null||id == 0)
+            {
+                return NotFound();
+            }
+
+            TodoList? todoListFromDb = _db.todoLists.Find(id);
+
+            if (todoListFromDb == null) 
+            {
+                return NotFound();
+            }
+            return View(todoListFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(TodoList obj)
+        {
+            if (obj.Title == null)
+            {
+                TempData["error"] = "請輸入標題";
+                return RedirectToAction("Edit");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.todoLists.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "代辦事項修改成功";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            TodoList todoListFromDb = _db.todoLists.Find(id);
+
+            if (todoListFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(todoListFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            TodoList? obj = _db.todoLists.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.todoLists.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "代辦事項刪除成功";
             return RedirectToAction("Index");
         }
     }
